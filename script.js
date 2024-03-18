6901e2c7e2f57745429c5702d2438d258de1366a
kevinsmuggles <63983530+kevinsmuggles@users.noreply.github.com>
2024-03-18 11:44:33 +0100

    added multi-hydra for script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function applyFilter(filterName) {
-    hydra.run(() => {
-      s0.initCam(2)
-    src(o0)
-.layer(src(s0).luma(.38).pixelate(200,200))
-.modulate(o0,.003)
-.colorama(()=>Math.sin(time*.1)*.2)
-.diff(src(o0).scale(.99).thresh(.18))
-.hue(.28)
-.out()
-      // Example filter
-        // Apply the specified filter based on filterName
-    });
-}
+let canvas1 = document.createElement("canvas");
+let canvas2 = document.createElement("canvas");
+canvas.width = 1024;
+canvas.height = 1024;
+canvas.id = "hydra-canvas";
+document.body.appendChild(canvas);
+const h = new Hydra({
+    canvas: canvas1,
+    detectAudio: false,
+    enableStreamCapture: false,
+}).synth
+h.osc(4, 0.1, 1.2).out()
+const h2 = new Hydra({
+    canvas: canvas1,
+    detectAudio: false,
+    enableStreamCapture: false,
+}).synth
+h2.noise().out()
